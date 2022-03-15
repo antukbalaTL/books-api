589664c39b8a8873b16269bba8d9b770284ba2e7
antukbalaTL <amit.kumar@trucklagbe.com>
2022-03-15 18:39:22 +0600

    morgan, body-parser added

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+
+
 const booksRoute = require('./api/routes/books');
 const usersRoute = require('./api/routes/users');
 const authorsRoute = require('./api/routes/authors');
 const categoriesRoute = require('./api/routes/categories');
+
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use(morgan('dev'));
+
 
 app.use('/books', booksRoute);
 app.use('/users', usersRoute);
